88c08ca1c7400b0cb051bd200ace80669789723a
Nicholas J. Michalek <djphazer@gmail.com>
2024-09-28 20:31:53 -0400

    fixup retrigger

--- src/ResamplingReader.h
+++ src/ResamplingReader.h
@@ -25,6 +25,7 @@ public:
     virtual TArray* createSourceBuffer(TFile& file) {return nullptr;};
     virtual int16_t getSourceBufferValue(long index) = 0;
     virtual void close(void) = 0;
+    virtual void retrig_process(void) { };
     int _lastInterpolationPosition[8] = {0};
 
     void begin(void)
@@ -406,28 +407,6 @@ private:
             // crossfade, so we have to do it ourselves here
             if (0 == _crossfadeState && _loopType == loop_type::looptype_pingpong)
                 _playbackRate = -_playbackRate;
-        }
-    }
-
-    void retrig_process(void) {
-        switch (_play_start)
-        {
-            case play_start::play_start_sample: // first audio block in file
-                _bufferPosition1 = _samples_to_start(0);
-                break;
-            case play_start::play_start_loop:   // loop start
-                Serial.printf("Retriggering loop, pos: %u", _loop_start);
-                _bufferPosition1 = _samples_to_start(_loop_start);
-                break;
-            case play_start::play_start_arbitrary: // user-defined position
-                _bufferPosition1 = _samples_to_start(_playback_start);
-                break;
-        }
-        if (nullptr != _sourceBuffer) {
-            if (_playbackRate >= 0.0f)
-                _sourceBuffer->preLoadBuffers(_bufferPosition1, _bufferInPSRAM);
-            else
-                _sourceBuffer->preLoadBuffers(_bufferPosition1, _bufferInPSRAM, false);
         }
     }
 
--- src/ResamplingSdReader.h
+++ src/ResamplingSdReader.h
@@ -58,6 +58,27 @@ public:
             _filename = nullptr;
         }
     }
+    void retrig_process(void) override {
+        switch (_play_start)
+        {
+            case play_start::play_start_sample: // first audio block in file
+                _bufferPosition1 = _samples_to_start(0);
+                break;
+            case play_start::play_start_loop:   // loop start
+                Serial.printf("Retriggering loop, pos: %u", _loop_start);
+                _bufferPosition1 = _samples_to_start(_loop_start);
+                break;
+            case play_start::play_start_arbitrary: // user-defined position
+                _bufferPosition1 = _samples_to_start(_playback_start);
+                break;
+        }
+        if (nullptr != _sourceBuffer) {
+            if (_playbackRate >= 0.0f)
+                _sourceBuffer->preLoadBuffers(_bufferPosition1, _bufferInPSRAM);
+            else
+                _sourceBuffer->preLoadBuffers(_bufferPosition1, _bufferInPSRAM, false);
+        }
+    }
 
     IndexableSDFile<BUFFER_SIZE_SD, BUFFER_COUNT_SD>* createSourceBuffer() override {
         File f = open(_filename);
