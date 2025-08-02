bc311085dcb4dbba3d2c8152b921c393e3b6fc6d
Nicholas J. Michalek <djphazer@gmail.com>
2025-08-02 17:55:40 -0400

    syncTrig only for forward playback

--- src/ResamplingReader.h
+++ src/ResamplingReader.h
@@ -803,7 +803,7 @@ public:
         setPlaybackRate(target / _tempo_bpm);
     }
     void syncTrig() {
-      if (isPlaying() && _tempo_bpm > 0.0) {
+      if (isPlaying() && _tempo_bpm > 0.0 && _playbackRate > 0.0) {
         const int samples_per_beat = AUDIO_SAMPLE_RATE_EXACT * 60 / _tempo_bpm;
         const int pos = getPosition() - ((_play_start == play_start::play_start_loop) * getLoopStart());
         int diff = pos % samples_per_beat;
